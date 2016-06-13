75305d923a862127b37180839d25a4150011e665
GABRIEL RODOLFO GOMES SANTIAGO <gabriellllllllll@gmail.com>
2016-06-12 23:12:25 -0300

    Add project files.

---
+++ BusinessDelegate/BusinessLookUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessDelegate
+{
+    public class BusinessLookUp
+    {
+        public IBusinessService getBusinessService(String serviceType)
+        {
+
+            if (serviceType.Equals("EJB"))
+            {
+                return new EJBService();
+            }
+            else
+            {
+                return new JMSService();
+            }
+        }
+    }
+}
---
+++ BusinessDelegate/JMSService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessDelegate
+{
+    class JMSService : IBusinessService
+    {
+        public void doProcessing()
+        {
+            Console.WriteLine("Processamento invocado pela JMS Service");
+        }
+    }
+}
